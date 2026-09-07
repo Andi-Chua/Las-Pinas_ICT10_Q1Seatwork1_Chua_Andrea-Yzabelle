11458509821b2f3172401db0a4ef76a43a976ca3
Andi-Chua <chua.ayt@obmontessori.edu.ph>
2026-09-07 20:30:59 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+#Chua, Ryon 
+studname = "Caleb Christopher Ryon" #string
+studage = 15 #interger
+studheight = 175 cm #float 
+stud_countries = ["USA", "England", "Canada"] #list 
+student_type = False #____
+stud_pref = { 
+    "color": "red",
+    "car brand": "Koeniggsegg"
+    "shoe size": 9, 
+    "bff": "Joaquin"
+    }
+stud_fruits = {
+    "Mango",
+    "Kyat-kyat",
+    "Green grapes",
+    "Apple",
+    "Cacao"
+    }
+days_week = (
+   "Monday",
+   "Tuesday",
+   "Wednesday",
+   "Thursday",
+   "Friday", 
+   "Saturday",
+   "Sunday"
+) #tuple 
+
+
